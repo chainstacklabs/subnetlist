a539af4a23d4100955f178e811cea6af7551d281
Antonio <tonioufa@gmail.com>
2022-03-28 12:17:34 +0200

    fixes font in card tittles

--- theme/coreTheme.js
+++ theme/coreTheme.js
@@ -1,11 +1,11 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles'
 
 export const colors = {
-  blue: "#ec4444",
+  blue: '#ec4444',
   red: '#ed4337',
   orange: '#ffb347',
-  lightBlack: 'rgba(0, 0, 0, 0.87)'
-};
+  lightBlack: 'rgba(0, 0, 0, 0.87)',
+}
 
 const coreTheme = {
   breakpoints: {
@@ -18,7 +18,7 @@ const coreTheme = {
     },
   },
   shape: {
-    borderRadius: '10px'
+    borderRadius: '10px',
   },
   typography: {
     fontFamily: [
@@ -35,62 +35,64 @@ const coreTheme = {
       '"Segoe UI Symbol"',
     ].join(','),
     h1: {
-      fontFamily: [
-        '"Helvetica Neue"',
-        'Inter',
-      ],
+      fontFamily: ['"Helvetica Neue"', 'Inter'],
       fontSize: '60px',
       fontWeight: 700,
       lineHeight: 1.167,
-      letterSpacing: '1px'
+      letterSpacing: '1px',
     },
     h2: {
       fontSize: '24px',
       fontWeight: 700,
-      lineHeight: 1.167
+      lineHeight: 1.167,
     },
     h3: {
-      fontFamily: [
-        'Inter',
-      ],
+      // fontFamily: [
+      //   'Inter',
+      // ],
       fontSize: '20px',
-      fontWeight: 600
+      fontWeight: 600,
     },
     h4: {
       fontSize: '1.5rem',
       fontWeight: 600,
-      lineHeight: 1.167
+      lineHeight: 1.167,
     },
-    h5: {   // card headers
+    h5: {
+      // card headers
       fontSize: '15px',
       fontWeight: 700,
-      lineHeight: 1.167
+      lineHeight: 1.167,
     },
-    h6: {   // card headers
+    h6: {
+      // card headers
       fontSize: '1.5rem',
       fontWeight: 700,
       lineHeight: 1.167,
       letterSpacing: '2px',
-      ['@media (max-width:576px)']: { // eslint-disable-line no-useless-computed-key
-        fontSize: '1.2rem'
-      }
+      ['@media (max-width:576px)']: {
+        // eslint-disable-line no-useless-computed-key
+        fontSize: '1.2rem',
+      },
     },
     subtitle1: {
       fontSize: '14px',
       fontWeight: 300,
       lineHeight: 1.167,
-      ['@media (max-width:576px)']: { // eslint-disable-line no-useless-computed-key
-        fontSize: '0.7rem'
-      }
+      ['@media (max-width:576px)']: {
+        // eslint-disable-line no-useless-computed-key
+        fontSize: '0.7rem',
+      },
     },
     body1: {
       fontSize: '16px',
       fontWeight: 500,
       lineHeight: 1.7,
-      ['@media (max-width:576px)']: { // eslint-disable-line no-useless-computed-key
-        fontSize: '0.8rem'
-      }
-    }
+      ['@media (max-width:576px)']: {
+        // eslint-disable-line no-useless-computed-key
+        fontSize: '0.8rem',
+      },
+    },
   },
   palette: {
     primary: {
@@ -100,136 +102,140 @@ const coreTheme = {
       main: '#EFEFEF',
     },
     error: {
-      main: '#dc3545'
-    }
+      main: '#dc3545',
+    },
   },
   overrides: {
     MuiPaper: {
       elevation1: {
-        "box-shadow": '0px 7px 7px #0000000A;',
-        "-webkit-box-shadow": '0px 7px 7px #0000000A;',
-        "-moz-box-shadow": '0px 7px 7px #0000000A;',
-      }
+        'box-shadow': '0px 7px 7px #0000000A;',
+        '-webkit-box-shadow': '0px 7px 7px #0000000A;',
+        '-moz-box-shadow': '0px 7px 7px #0000000A;',
+      },
     },
     MuiOutlinedInput: {
       input: {
-        padding: '12.5px 14px'
-      }
+        padding: '12.5px 14px',
+      },
     },
     MuiTooltip: {
       tooltip: {
         maxWidth: 'none',
-      }
+      },
     },
     MuiButton: {
       sizeLarge: {
         borderRadius: '50px',
         width: '260px',
-        height: '60px'
+        height: '60px',
       },
       root: {
         minWidth: '50px',
-        borderRadius: '32px'
+        borderRadius: '32px',
       },
       outlinedSizeSmall: {
         fontSize: '0.7rem',
         padding: '6px 9px',
-        ['@media (max-width:576px)']: { // eslint-disable-line no-useless-computed-key
+        ['@media (max-width:576px)']: {
+          // eslint-disable-line no-useless-computed-key
           padding: '3px 0px',
-        }
+        },
       },
       textSizeLarge: {
         fontSize: '2.4rem',
-        ['@media (max-width:576px)']: { // eslint-disable-line no-useless-computed-key
-          fontSize: '2rem'
-        }
+        ['@media (max-width:576px)']: {
+          // eslint-disable-line no-useless-computed-key
+          fontSize: '2rem',
+        },
       },
       label: {
-        textTransform: 'none'
+        textTransform: 'none',
       },
       outlinedPrimary: {
         border: '1px solid #EAEAEA',
-        "&:hover": {
-          backgroundColor: colors.blue+' !important',
-          color: '#fff'
-        }
-      }
+        '&:hover': {
+          backgroundColor: colors.blue + ' !important',
+          color: '#fff',
+        },
+      },
     },
     MuiDialog: {
       paperWidthSm: {
-        maxWidth: '800px'
-      }
+        maxWidth: '800px',
+      },
     },
     MuiToggleButton: {
       root: {
         flex: 1,
-        padding: '9px 6px'
-      }
+        padding: '9px 6px',
+      },
     },
-    MuiSnackbar : {
+    MuiSnackbar: {
       root: {
-        maxWidth: 'calc(100vw - 24px)'
+        maxWidth: 'calc(100vw - 24px)',
       },
       anchorOriginBottomLeft: {
         bottom: '12px',
         left: '12px',
         '@media (min-width: 960px)': {
           bottom: '50px',
-          left: '80px'
-        }
-      }
+          left: '80px',
+        },
+      },
     },
     MuiInputBase: {
       input: {
-        fontSize: '14px'
+        fontSize: '14px',
       },
       root: {
         margin: '0px',
-        '&:before': { //underline color when textfield is inactive
+        '&:before': {
+          //underline color when textfield is inactive
           borderBottom: 'none !important',
-          height: '0px'
+          height: '0px',
         },
-        '&:after': { //underline color when textfield is inactive
+        '&:after': {
+          //underline color when textfield is inactive
           borderBottom: 'none !important',
-          height: '0px'
+          height: '0px',
         },
-      }
+      },
     },
     MuiAccordion: {
       root: {
         margin: '0px',
-        '&:before': { //underline color when textfield is inactive
+        '&:before': {
+          //underline color when textfield is inactive
           backgroundColor: 'none',
-          height: '0px'
+          height: '0px',
         },
         '&$expanded': {
-          margin: '0px'
-        }
-      }
+          margin: '0px',
+        },
+      },
     },
     MuiAccordionSummary: {
       root: {
         padding: '0px 24px',
         '@media (max-width:576px)': {
           padding: '0px 6px',
-        }
+        },
       },
       content: {
-        margin: '0px !important'
-      }
+        margin: '0px !important',
+      },
     },
     MuiAccordionDetails: {
       root: {
         padding: '0',
-      }
+      },
     },
     MuiFormHelperText: {
       contained: {
-        textAlign: 'end'
-      }
-    }
-  }
+        textAlign: 'end',
+      },
+    },
+  },
 }
 
-
-export default coreTheme;
+export default coreTheme
