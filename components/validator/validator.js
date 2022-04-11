759d4809994f7270a0b3fad924c5db7e4d332826
Antonio <tonioufa@gmail.com>
2022-04-11 16:20:47 +0100

    improved design

--- components/validator/validator.js
+++ components/validator/validator.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect, useMemo } from 'react'
 
 import classes from './validator.module.css'
 import { Typography, Paper, Link } from '@material-ui/core'
+
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn'
+import ReceiptIcon from '@material-ui/icons/Receipt'
+
 export default function Validator({ validator }) {
   return (
     <Paper elevation={1} key={validator.nodeID}>
@@ -9,16 +13,24 @@ export default function Validator({ validator }) {
         <Typography variant="h3" className={classes.name}>
           {validator.nodeID}
         </Typography>
-        <Typography>
-          💰 Total stake: {validator.totalStake} {''} AVAX
-        </Typography>
-        <Typography>
-          🧾 Fee: {validator.feePercentage}
-          {'%'} AVAX
-        </Typography>
-        {validator.openToValidate && (
-          <Link href={validator.contactDetails.email}>Contact</Link>
-        )}
+        <div className={classes.validatorInfoContainer}>
+          <div className={classes.rowInfo}>
+            <MonetizationOnIcon color="primary" />
+            <Typography>
+              {validator.totalStake} {'AVAX total stake'}
+            </Typography>
+          </div>
+          <div className={classes.rowInfo}>
+            <ReceiptIcon color="primary" />
+            <Typography align-content="center">
+              {validator.feePercentage}
+              {'% validator fee'}
+            </Typography>
+          </div>
+          {validator.openToValidate && (
+            <Link href={validator.contactDetails.email}>Contact</Link>
+          )}
+        </div>
       </div>
     </Paper>
   )
